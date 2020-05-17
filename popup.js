9c78e9ab0b8a302abc1f8ca35aae880bbdf938df
Andre Pires <andre.rua.pires@gmail.com>
2020-05-17 09:57:55 +0100

    Bug found, not covering all cookies. Major refactor covers all cookies and simpler way to delete both cookies and localstorage.

--- popup.js
+++ popup.js
@@ -5,28 +5,39 @@ clearButton.addEventListener("click", async () => {
         "active": true,
         "currentWindow": true
     }
-    
+
     await chrome.tabs.query(tabInfo, (tab) => {
-        const currentURL = tab[0].url;
-        const origin = currentURL.match(/^[\w-]+:\/{2,}\[?[\w\.:-]+\]?(?::[0-9]*)?/)[0];
-        const details = {
-            "url": origin
-        }
-        chrome.cookies.getAll(details, (cookies) => {
-            for(cookie of cookies) {
-                let cookieDetails = {
-                    "url": origin,
-                    "name": cookie.name
+
+        const currentURL = tab[0].url;                                                  // current tab url
+
+        chrome.tabs.executeScript({
+            code: 'performance.getEntriesByType("resource").map(e => e.name)',          // fetch info about the navigation performance
+        }, data => {                                                                    // in this case, the property "name" has URLs that this page utilizes
+            if (chrome.runtime.lastError || !data || !data[0]) return;                  // Error case
+
+            const urls = data[0].map(url => url.split(/[#?]/)[0]);                      // get url without hash
+            urls.push(currentURL);                                                        
+
+            const domains = urls.reduce((result, url) => {                              // filter http & https urls
+                let match = url.match(/^[\w-]+:\/{2,}\[?[\w\.:-]+\]?(?::[0-9]*)?/);     // only origin
+                if(match != null) {
+                    result.push(match[0]);
                 }
-                chrome.cookies.remove(cookieDetails)
-            }
-            chrome.tabs.executeScript({
-                code: "localStorage.clear()"
+                return result;
+            }, []);
+            const uniqueUrls = [...new Set(domains).values()].filter(Boolean);          // filter to get unique origins
+            uniqueUrls.push(origin);                                                    // push current tab origin to array
+
+            chrome.browsingData.remove({                                                // remove all cookies and localStorage related to the origins
+                "origins": uniqueUrls
+              }, {
+                "cookies": true,
+                "localStorage": true
             });
 
-            chrome.tabs.reload();
-
-            window.close();
+            chrome.tabs.reload();                                                       // reload page
+    
+            window.close();                                                             // close popup
         });
     });
 });
